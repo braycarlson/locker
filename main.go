53a7667180877cb0384b1a89d217d5d037c29439
Brayden Carlson <braycarlson@hotmail.com>
2024-07-22 22:08:11 -0600

    Create a .dll for global hooking

--- main.go
+++ main.go
@@ -12,27 +12,33 @@ import (
 	"github.com/getlantern/systray"
 )
 
+/*
+#cgo LDFLAGS: -Ldll -lhook
+#include "dll/hook.h"
+*/
+import "C"
+
 const (
 	Context = 27
 	Escape  = 93
 )
 
 var (
-	mouseEvent = map[w32.WPARAM]bool{
-		w32.WPARAM(w32.WM_LBUTTONDOWN): true,
-		w32.WPARAM(w32.WM_MBUTTONDOWN): true,
-		w32.WPARAM(w32.WM_RBUTTONDOWN): true,
-		w32.WPARAM(w32.WM_XBUTTONDOWN): true,
-		w32.WPARAM(w32.WM_LBUTTONUP):   true,
-		w32.WPARAM(w32.WM_MBUTTONUP):   true,
-		w32.WPARAM(w32.WM_RBUTTONUP):   true,
-		w32.WPARAM(w32.WM_XBUTTONUP):   true,
-		w32.WPARAM(w32.WM_MOUSEWHEEL):  true,
+	mouseEvent = map[C.WPARAM]bool{
+		C.WPARAM(w32.WM_LBUTTONDOWN): true,
+		C.WPARAM(w32.WM_MBUTTONDOWN): true,
+		C.WPARAM(w32.WM_RBUTTONDOWN): true,
+		C.WPARAM(w32.WM_XBUTTONDOWN): true,
+		C.WPARAM(w32.WM_LBUTTONUP):   true,
+		C.WPARAM(w32.WM_MBUTTONUP):   true,
+		C.WPARAM(w32.WM_RBUTTONUP):   true,
+		C.WPARAM(w32.WM_XBUTTONUP):   true,
+		C.WPARAM(w32.WM_MOUSEWHEEL):  true,
 	}
 
-	keyboardEvent = map[w32.WPARAM]bool{
-		w32.WPARAM(w32.WM_KEYDOWN):    true,
-		w32.WPARAM(w32.WM_SYSKEYDOWN): true,
+	keyboardEvent = map[C.WPARAM]bool{
+		C.WPARAM(w32.WM_KEYDOWN):    true,
+		C.WPARAM(w32.WM_SYSKEYDOWN): true,
 	}
 
 	keypressEvent = map[w32.DWORD]bool{
@@ -50,14 +56,12 @@ var (
 type Locker struct {
 	LockHotkey   []byte
 	UnlockHotkey []byte
-	keyboard     w32.HHOOK
-	mouse        w32.HHOOK
 	queue        *buffer.CircularBuffer
 	lock         []byte
 	unlock       []byte
-	resource     []byte
 	logger       chan string
 	shutdown     chan bool
+	isLocked     bool
 }
 
 func NewLocker() *Locker {
@@ -72,6 +76,67 @@ func NewLocker() *Locker {
 	}
 }
 
+var locker *Locker
+
+//export GoKeyboardProc
+func GoKeyboardProc(nCode C.int, wParam C.WPARAM, lParam C.LPARAM) C.LRESULT {
+	if nCode >= 0 {
+		if wParam == C.WPARAM(w32.WM_KEYDOWN) || wParam == C.WPARAM(w32.WM_SYSKEYDOWN) {
+			var message unsafe.Pointer = unsafe.Pointer(uintptr(lParam))
+			var kbdstruct *w32.KBDLLHOOKSTRUCT = (*w32.KBDLLHOOKSTRUCT)(message)
+
+			var key byte = byte(kbdstruct.VkCode)
+			locker.queue.Push(key)
+
+			if locker.queue.IsMatch(locker.LockHotkey) {
+				locker.logger <- "Attempting to lock..."
+				systray.SetIcon(locker.lock)
+				locker.toggleHook(true)
+				return 1
+			}
+
+			if locker.queue.IsMatch(locker.UnlockHotkey) {
+				locker.logger <- "Attempting to unlock..."
+				systray.SetIcon(locker.unlock)
+				locker.toggleHook(false)
+				return 1
+			}
+		}
+
+		if locker.isLocked && keyboardEvent[wParam] {
+			return 1
+		}
+	}
+
+	return C.callNextHookEx(nCode, wParam, lParam)
+}
+
+//export GoMouseProc
+func GoMouseProc(nCode C.int, wParam C.WPARAM, lParam C.LPARAM) C.LRESULT {
+	if nCode >= 0 && locker.isLocked && mouseEvent[wParam] {
+		return 1
+	}
+
+	return C.callNextHookEx(nCode, wParam, lParam)
+}
+
+func (locker *Locker) toggleHook(lock bool) {
+	if lock {
+		locker.logger <- "Locking input"
+		locker.isLocked = true
+		C.setGoKeyboardProc(C.HOOKPROC(C.GoKeyboardProc))
+		C.setGoMouseProc(C.HOOKPROC(C.GoMouseProc))
+		C.setKeyboardHook(C.getModuleHandle())
+		C.setMouseHook(C.getModuleHandle())
+	} else {
+		locker.logger <- "Unlocking input"
+		locker.isLocked = false
+		C.removeHook()
+		C.setGoKeyboardProc(C.HOOKPROC(C.GoKeyboardProc))
+		C.setKeyboardHook(C.getModuleHandle())
+	}
+}
+
 func (locker *Locker) logging() {
 	for {
 		select {
@@ -79,110 +144,11 @@ func (locker *Locker) logging() {
 			if !ok {
 				return
 			}
-
 			log.Println(message)
 		case <-locker.shutdown:
 			close(locker.logger)
 			return
 		}
-	}
-}
-
-func (locker *Locker) listener(identifier int, wparam w32.WPARAM, lparam w32.LPARAM) w32.LRESULT {
-	if wparam == w32.WPARAM(w32.WM_KEYDOWN) || wparam == w32.WPARAM(w32.WM_SYSKEYDOWN) {
-		var message unsafe.Pointer = unsafe.Pointer(lparam)
-		var kbdstruct *w32.KBDLLHOOKSTRUCT = (*w32.KBDLLHOOKSTRUCT)(message)
-
-		var key byte = byte(kbdstruct.VkCode)
-		locker.queue.Push(key)
-
-		if locker.queue.IsMatch(locker.LockHotkey) {
-			locker.logger <- "Attempting to lock..."
-			systray.SetIcon(locker.lock)
-			locker.toggleHook(true)
-		}
-	}
-
-	return w32.CallNextHookEx(
-		0,
-		identifier,
-		wparam,
-		lparam,
-	)
-}
-
-func (locker *Locker) blockMouse(identifier int, wparam w32.WPARAM, lparam w32.LPARAM) w32.LRESULT {
-	if mouseEvent[wparam] {
-		return 1
-	}
-
-	return w32.CallNextHookEx(
-		0,
-		identifier,
-		wparam,
-		lparam,
-	)
-}
-
-func (locker *Locker) blockKeyboard(identifier int, wparam w32.WPARAM, lparam w32.LPARAM) w32.LRESULT {
-	if keyboardEvent[wparam] {
-		return 1
-	}
-
-	if wparam == w32.WPARAM(w32.WM_KEYUP) || wparam == w32.WPARAM(w32.WM_SYSKEYUP) {
-		var message unsafe.Pointer = unsafe.Pointer(lparam)
-		var kbdstruct *w32.KBDLLHOOKSTRUCT = (*w32.KBDLLHOOKSTRUCT)(message)
-
-		// Disable the esc and context menu
-		if keypressEvent[kbdstruct.VkCode] {
-			return 1
-		}
-
-		var key byte = byte(kbdstruct.VkCode)
-		locker.queue.Push(key)
-
-		if locker.queue.IsMatch(locker.UnlockHotkey) {
-			locker.logger <- "Attempting to unlock..."
-			systray.SetIcon(locker.unlock)
-			locker.toggleHook(false)
-
-			return 0
-		}
-	}
-
-	return w32.CallNextHookEx(
-		0,
-		identifier,
-		wparam,
-		lparam,
-	)
-}
-
-func (locker *Locker) toggleHook(lock bool) {
-	w32.UnhookWindowsHookEx(locker.keyboard)
-	w32.UnhookWindowsHookEx(locker.mouse)
-
-	if lock {
-		locker.keyboard = w32.SetWindowsHookEx(
-			w32.WH_KEYBOARD_LL,
-			w32.HOOKPROC(locker.blockKeyboard),
-			0,
-			0,
-		)
-
-		locker.mouse = w32.SetWindowsHookEx(
-			w32.WH_MOUSE_LL,
-			w32.HOOKPROC(locker.blockMouse),
-			0,
-			0,
-		)
-	} else {
-		locker.keyboard = w32.SetWindowsHookEx(
-			w32.WH_KEYBOARD_LL,
-			w32.HOOKPROC(locker.listener),
-			0,
-			0,
-		)
 	}
 }
 
@@ -209,26 +175,29 @@ func (locker *Locker) onReady() {
 func (locker *Locker) onExit() {
 	locker.logger <- "Exiting..."
 
-	w32.UnhookWindowsHookEx(locker.keyboard)
-	w32.UnhookWindowsHookEx(locker.mouse)
+	C.removeHook()
 
 	locker.shutdown <- true
 	close(locker.shutdown)
 }
 
 func (locker *Locker) run() {
-	locker.keyboard = w32.SetWindowsHookEx(
-		w32.WH_KEYBOARD_LL,
-		w32.HOOKPROC(locker.listener),
-		0,
-		0,
-	)
+	C.setGoKeyboardProc(C.HOOKPROC(C.GoKeyboardProc))
+	C.setGoMouseProc(C.HOOKPROC(C.GoMouseProc))
+	C.setKeyboardHook(C.getModuleHandle())
 
 	locker.logger <- "Entering message loop..."
 
 	var message w32.MSG
 
-	for w32.GetMessage(&message, 0, 0, 0) != 0 {
+	for {
+		var status int
+		status = w32.GetMessage(&message, 0, 0, 0)
+
+		if status == 0 || status == -1 {
+			break
+		}
+
 		w32.TranslateMessage(&message)
 		w32.DispatchMessage(&message)
 	}
@@ -261,6 +230,6 @@ func main() {
 
 	defer file.Close()
 
-	var locker *Locker = NewLocker()
+	locker = NewLocker()
 	systray.Run(locker.onReady, locker.onExit)
 }
